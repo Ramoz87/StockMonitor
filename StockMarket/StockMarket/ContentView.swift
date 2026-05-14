f41f1630f373bd07da7d02ac3bf93a1fb6bcfa68
Yury Ramazanov <yury.ramazanov@gmail.com>
2026-05-14 10:09:57 +0200

    StockMarket app project

---
+++ StockMarket/StockMarket/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  StockMarket
+//
+//  Created by Yury Ramazanov on 14.05.2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ StockMarket/StockMarket/StockMarketApp.swift
@@ -0,0 +1,17 @@
+//
+//  StockMarketApp.swift
+//  StockMarket
+//
+//  Created by Yury Ramazanov on 14.05.2026.
+//
+
+import SwiftUI
+
+@main
+struct StockMarketApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
